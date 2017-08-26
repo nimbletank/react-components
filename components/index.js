c5b7349a1d22dea41e60e4bf9a7b95244a82dd08
Zander Martineau <zander@martineau.tv>
2017-08-26 09:54:34 +0100

    move index file into `components` dir

---
+++ components/index.js
@@ -0,0 +1,20 @@
+// Style-agnostic components
+import Element from './Element'
+import Container from './Container'
+import Button from './Button'
+import Heading, { H1, H2, H3, H4, H5, H6, Title, Subtitle } from './Heading'
+
+export {
+  Element,
+  Container,
+  Button,
+  Heading,
+  H1,
+  H2,
+  H3,
+  H4,
+  H5,
+  H6,
+  Title,
+  Subtitle
+}
