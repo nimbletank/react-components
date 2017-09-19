04e45925d9f546467c561549f765d5ad287a45e8
Zander Martineau <zander@martineau.tv>
2017-09-19 07:56:38 +0100

    convert the Button component to use `emotion`

--- .storybook/stories/Button.js
+++ .storybook/stories/Button.js
@@ -12,7 +12,7 @@ import JSXAddon from 'storybook-addon-jsx'
 import withReadme from 'storybook-readme/with-readme'
 
 // Components
-import Button from '../../components/Button'
+import { Button, ButtonPrimary } from '../../components/Button'
 import ButtonReadMe from '../../components/Button/readme.md'
 
 setAddon(JSXAddon)
@@ -25,6 +25,7 @@ stories
     withReadme(ButtonReadMe, () => (
       <Button
         disabled={boolean('Disabled', false)}
+        block={boolean('Full-width button', false)}
         thinking={boolean('Thinking', false)}
       >
         {text('Button text', 'Default Button')}
@@ -34,50 +35,50 @@ stories
   .addWithJSX(
     'Primary button',
     withReadme(ButtonReadMe, () => (
-      <Button
-        primary
+      <ButtonPrimary
         disabled={boolean('Disabled', false)}
+        block={boolean('Full-width button', false)}
         thinking={boolean('Thinking', false)}
       >
-        {text('Button text', 'Primary Button')}
-      </Button>
+        {text('Button text', 'Default Button')}
+      </ButtonPrimary>
     ))
   )
-  .addWithJSX(
-    'Secondary button',
-    withReadme(ButtonReadMe, () => (
-      <Button
-        secondary
-        disabled={boolean('Disabled', false)}
-        thinking={boolean('Thinking', false)}
-      >
-        {text('Button text', 'Secondary Button')}
-      </Button>
-    ))
-  )
-  .addWithJSX(
-    'Block button',
-    withReadme(ButtonReadMe, () => (
-      <Button
-        block
-        primary
-        disabled={boolean('Disabled', false)}
-        thinking={boolean('Thinking', false)}
-      >
-        {text('Button text', 'Block Button')}
-      </Button>
-    ))
-  )
-  .addWithJSX(
-    'Link button',
-    withReadme(ButtonReadMe, () => (
-      <Button
-        tag="a"
-        disabled={boolean('Disabled', false)}
-        thinking={boolean('Thinking', false)}
-        href="https://nimbletank.com"
-      >
-        {text('Button text', 'Link button')}
-      </Button>
-    ))
-  )
+  // .addWithJSX(
+  //   'Secondary button',
+  //   withReadme(ButtonReadMe, () => (
+  //     <Button
+  //       secondary
+  //       disabled={boolean('Disabled', false)}
+  //       thinking={boolean('Thinking', false)}
+  //     >
+  //       {text('Button text', 'Secondary Button')}
+  //     </Button>
+  //   ))
+  // )
+  // .addWithJSX(
+  //   'Block button',
+  //   withReadme(ButtonReadMe, () => (
+  //     <Button
+  //       block
+  //       primary
+  //       disabled={boolean('Disabled', false)}
+  //       thinking={boolean('Thinking', false)}
+  //     >
+  //       {text('Button text', 'Block Button')}
+  //     </Button>
+  //   ))
+  // )
+  // .addWithJSX(
+  //   'Link button',
+  //   withReadme(ButtonReadMe, () => (
+  //     <Button
+  //       tag="a"
+  //       disabled={boolean('Disabled', false)}
+  //       thinking={boolean('Thinking', false)}
+  //       href="https://nimbletank.com"
+  //     >
+  //       {text('Button text', 'Link button')}
+  //     </Button>
+  //   ))
+  // )
---
+++ components/Button/Button.js
@@ -0,0 +1,119 @@
+/**
+ * Button component
+ * Generic button component
+ *
+ *
+ * import {Button} from '@nimbletank/react-components'
+ *  or
+ * import Button from '@nimbletank/react-components/dist/Button'
+ *
+ *
+ * Additional boolean attributes allow for other specific classes to be added:
+ * `block` > `.Button--block`
+ *
+ * @param {string} className - Additional class for the button
+ * @param {func} onClick - onClick function for the button
+ *
+ * Usage: <Button className="foo-bar" onClick={handleClick}>Foo Bar</Button>
+ * Output: <button class="Button foo-bar">Foo Bar</button>
+ *
+ * Usage: <ButtonPrimaryLink href="https://nimbletank.com" onClick={handleClick}>Foo Bar</ButtonPrimaryLink>
+ * Output: <a href="https://nimbletank.com">Foo Bar</a>
+ *
+ */
+import { css } from 'emotion'
+import styled from 'react-emotion'
+
+/**
+ * Shared button styles
+ */
+const ButtonCSS = (props) => css`
+  display: ${props => props.block ? 'block' : 'inline-block'};
+  width: ${props => props.block ? '100%' : 'auto'};
+  vertical-align: middle;
+  white-space: nowrap;
+  font-family: inherit;
+  font-size: 100%;
+  cursor: pointer;
+  border: none;
+  margin: 0;
+  padding-top: 0;
+  padding-bottom: 0;
+  line-height: 2.5;
+  height: 2.5em;
+  padding-right: 1.5em;
+  padding-left: 1.5em;
+  overflow: visible;
+  text-align: center;
+  border-radius: 5px;
+  user-select: none;
+  text-decoration: none;
+  transition: all 300ms cubic-bezier(0.77, 0, 0.175, 1); /* Var */
+
+  &[disabled] {
+    cursor: default;
+    background-image: none;
+    opacity: .5;
+  }
+`
+
+/**
+ * Default button
+ */
+const ButtonStyles = css`
+  ${ButtonCSS};
+
+  &,
+  &:link,
+  &:visited {
+    background-color: #EDEDED; /* Var */
+    color: #444; /* Var */
+  }
+
+  &:hover,
+  &:active,
+  &:focus {
+    background-color: #D5D5D5; /* Var */
+    outline: none;
+  }
+`
+
+export const Button = styled.button`
+  ${ButtonCSS};
+  ${ButtonStyles};
+`
+
+export const ButtonLink = styled.a`
+  ${ButtonCSS};
+  ${ButtonStyles};
+`
+
+/**
+ * Primary button
+ */
+const ButtonPrimaryStyles = css`
+  ${ButtonCSS};
+
+  &,
+  &:link,
+  &:visited {
+    background-color: #3FC1C9; /* Var */
+    color: #fff; /* Var */
+  }
+
+  &:hover,
+  &:active,
+  &:focus {
+    background-color: #2B8D93; /* Var */
+  }
+`
+
+export const ButtonPrimary = styled.button`
+  ${ButtonCSS};
+  ${ButtonPrimaryStyles};
+`
+
+export const ButtonPrimaryLink = styled.a`
+  ${ButtonCSS};
+  ${ButtonPrimaryStyles};
+`
