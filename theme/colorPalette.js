da0cc2207ffffee1049440e01dbfe9422907011b
Zander Martineau <zander.martineau@nimbletank.com>
2017-12-13 13:10:26 +0000

    add a design-system-utils theme

---
+++ theme/colorPalette.js
@@ -0,0 +1,47 @@
+export default {
+  bright: {
+    base: '#F9FAFB',
+    dark: '#F4F6F8',
+    darker: '#DFE4E8',
+  },
+
+  dark: {
+    lighter: '#637381',
+    light: '#454F5B',
+    base: '#212B35',
+  },
+
+  primary: {
+    light: '#292952',
+    base: '#181830',
+    dark: '#0d0d19',
+  },
+
+  secondary: {
+    light: '#fea04c',
+    base: '#fe7c08',
+    dark: '#d26401',
+  },
+
+  text: {
+    light: '#454F5B',
+    base: '#212B35',
+  },
+
+  background: {
+    base: '#f1f1f1',
+  },
+
+  link: {
+    base: '#fe7c08',
+    over: '#fea04c',
+  },
+
+  blue: {
+    lighter: '#EBF5FA',
+    light: '#B4E1FA',
+    base: '#007ACE',
+    dark: '#084E8A',
+    darker: '#001429',
+  },
+}
